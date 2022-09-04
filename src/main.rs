55ab92325e1a8b2a665296f97cb3688bfda6bffd
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2022-09-05 07:38:49 +1000

    add cli arg parsing with Clap
    Changes:
    - require filepath arg

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,38 @@ mod display;
 mod keyboard;
 
 use chip_8::Chip8;
+use clap::Parser;
 use display::Display;
 use keyboard::Keyboard;
 use sdl2::event::Event;
 use sdl2::keyboard::Scancode;
 use std::cell::RefCell;
+use std::fs;
+use std::path::PathBuf;
 use std::process;
 use std::rc::Rc;
 
 const WINDOW_WIDTH: u32 = 64;
 const WINDOW_HEIGHT: u32 = 32;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Config {
+    #[clap(
+        short,
+        long,
+        value_parser,
+        value_name = "FILE",
+        help = "ROM file to be loaded into Chip-8"
+    )]
+    file: PathBuf,
+}
+
 fn main() {
+    let config = Config::parse();
+
+    let rom = fs::read(config.file).unwrap();
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
@@ -32,7 +52,7 @@ fn main() {
     let display = Display::new(Rc::clone(&canvas));
     let mut c8 = Chip8::new(keyboard, display);
 
-    c8.load(&[0x12, 0x00]);
+    c8.load(&rom);
 
     loop {
         for event in event_pump.borrow_mut().poll_iter() {
